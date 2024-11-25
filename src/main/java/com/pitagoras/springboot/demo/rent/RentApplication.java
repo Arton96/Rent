e3448b58e5f5a4fa7685fb0585474c7313bba53b
Arton <artonhasani96@gmail.com>
2024-11-25 19:57:19 +0100

    Crud operation

--- src/main/java/com/pitagoras/springboot/demo/rent/RentApplication.java
+++ src/main/java/com/pitagoras/springboot/demo/rent/RentApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RentApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(RentApplication.class, args);
+		System.out.println("hello world");
 	}
 
 }
--- src/main/java/com/pitagoras/springboot/demo/rent/rest/CarController.java
+++ src/main/java/com/pitagoras/springboot/demo/rent/rest/CarController.java
@@ -1,20 +1,36 @@
 package com.pitagoras.springboot.demo.rent.rest;
 
+import com.pitagoras.springboot.demo.rent.dao.CarDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pitagoras.springboot.demo.rent.entity.Car;
+
 @RestController
 public class CarController {
-    private Car myCar;
 
+    //private Car myCar;
+    private CarDAO carDAO;
     @Autowired
-    public CarController(Car car){
-        this.myCar = car;
-    }
+    public CarController(CarDAO carDAO){
+        this.carDAO = carDAO;
+      }
 
-    @GetMapping("/drive")
-    public String driveMyCar(){
-        return this.myCar.drive();
+
+    @GetMapping("/save-car")
+    public String save(){
+        Car car = new Car();
+        car.setModel("M3");
+        car.setMake("BMW");
+        car.setColor("RED");
+         this.carDAO.save(car);
+
+         return "Car was created succesfully " + car.getId();
+    }
+    @GetMapping("/find-car")
+    public String findMyCar(){
+        return this.carDAO.findById(2).toString();
     }
 }
